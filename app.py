9c356c8cfefc205ff3e8aa96e5bb7f3e024c4f0f
Hrishikesh Karanth Puttur <hrish007@umd.edu>
2024-08-28 01:04:08 -0400

    app.py is done

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 API_KEY = "ad96171a3f768a4396124d737403ac02"
 
 def get_weather(city):
-    url = f'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API_key}'
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}'
     response = requests.get(url)
     return response.json()
 
@@ -18,11 +18,16 @@ def index():
         city = request.form.get('city')
         weather_data =  get_weather(city)
 
+        if weather_data and 'main' not in weather_data:
+
+            error_message = weather_data.get("message","City not found or some other error")
+            return render_template('index.html',weather = weather_data, error = error_message)
+
     return render_template('index.html', weather=weather_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
-    
+
 
 
              
